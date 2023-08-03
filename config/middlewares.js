9a1c3b4c979fa562ca0c08b8ada715711151510e
Hammad Ahmed <30lazers@gmail.com>
2023-08-03 14:03:37 +0500

    Backend First Commit #1

---
+++ config/middlewares.js
@@ -0,0 +1,18 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*']
+    }
+  },
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
